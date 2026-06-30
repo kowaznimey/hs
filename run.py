0a3545d44a11de52024a14d25789e1dbdf6c14a7
kowaznimey <ko.waznimey@googlemail.com>
2026-06-30 19:23:15 +0630

    Update run.py

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ from datetime import datetime, timedelta, timezone
 # ── Environment variables (သင်ကိုယ်တိုင် ပြန်လည် အစားထိုးရန်) ─────────────────
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8915737207:AAFymbnH_1Ga39WrcoRmfhca73hGhwF4kzY")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN","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")
-REPO_OWNER = os.getenv("REPO_OWNER", "Allibarbar")
-REPO_NAME = os.getenv("REPO_NAME", "Alli")
-ADMIN_ID = os.getenv("ADMIN_ID", "5406128711") # သင့်ရဲ့ ID ကို ဒီမှာ ထည့်ပါ
+REPO_OWNER = os.getenv("REPO_OWNER", "kowaznimey")
+REPO_NAME = os.getenv("REPO_NAME", "hs")
+ADMIN_ID = os.getenv("ADMIN_ID", "5671920054") # သင့်ရဲ့ ID ကို ဒီမှာ ထည့်ပါ
 
 # ── Global structures ─────────────────────────────────────────────────────
 SUCCESS_CODE = asyncio.Queue()
